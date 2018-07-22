5b077e7f31e75afc2edb97715da6a0d79b2de589
Vicent Mereti <keshmannn@gmail.com>
2018-07-22 13:11:25 +0400

    init with androidx, material, realm

---
+++ app/src/main/java/io/github/kmeret/demo/App.kt
@@ -0,0 +1,14 @@
+package io.github.kmeret.demo
+
+import android.app.Application
+import io.realm.Realm
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+    }
+
+}
---
+++ app/src/main/java/io/github/kmeret/demo/navigation/NavActivity.kt
@@ -0,0 +1,13 @@
+package io.github.kmeret.demo.navigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.kmeret.demo.R
+
+class NavActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav)
+    }
+}
