a31ccaf7522a822ad15b7d74bd921982ea14d0bc
Vicent Mereti <keshmannn@gmail.com>
2018-11-28 14:46:09 +0400

    ProcessLifecycleOwner

--- demo/src/main/java/io/github/kmeret/frame/demo/App.kt
+++ demo/src/main/java/io/github/kmeret/frame/demo/App.kt
@@ -1,7 +1,9 @@
 package io.github.kmeret.frame.demo
 
 import android.app.Application
+import androidx.lifecycle.ProcessLifecycleOwner
 import io.github.kmeret.frame.demo.di.*
+import io.github.kmeret.frame.demo.navigation.ToastLiveData
 import org.koin.android.ext.android.startKoin
 
 class App : Application() {
@@ -9,6 +11,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initDI()
+        ToastLiveData(this, ProcessLifecycleOwner.get().lifecycle)
     }
 
     private fun initDI() = startKoin(this, listOf(
